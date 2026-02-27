6285cbc00db88cdc856bfd4071694b619198ec18
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2026-02-27 15:58:56 +0100

    ADD: [Assets] Assets

---
+++ assets/assets/custom/fullbright/unlit.glsl
@@ -0,0 +1,66 @@
+#vertex_shader
+        
+#version 330 core
+
+layout (location = POSITION_ATTRIBUTE_POINTER) in vec3 aPos;
+layout (location = NORMAL_ATTRIBUTE_POINTER) in vec3 aNormal;
+layout (location = TEXCOORD_ATTRIBUTE_POINTER) in vec2 aTexCoord;
+
+// These should not be uniforms.
+uniform mat4 View;
+uniform mat4 Projection;
+uniform mat4 Model;
+
+// Shadow Mapping
+uniform mat4 LightSpaceMatrix;
+
+// TODO: Out block
+out vec3 FragPos;
+out vec2 TexCoord;
+out vec3 Normal;
+out vec4 FragPosLightSpace;
+
+void main()
+{
+    // Outputs
+    Normal = mat3(transpose(inverse(Model))) * aNormal;
+    TexCoord = aTexCoord;
+    FragPos = (Model * vec4(aPos, 1.0)).xyz;
+    FragPosLightSpace = LightSpaceMatrix * vec4(FragPos, 1.0);
+    
+    gl_Position = Projection * View * Model * vec4(aPos, 1.0);
+}
+        
+#fragment_shader
+
+#version 330 core
+
+#include <includes/lights.common>
+
+in vec3 FragPos;
+in vec2 TexCoord;
+in vec3 Normal;
+in vec4 FragPosLightSpace;
+
+out vec4 FragColor;
+
+uniform vec3 viewPosition;
+
+uniform float specularStrength;
+uniform int shininess;
+
+uniform sampler2D ALBEDO_TEXTURE;
+uniform sampler2D NORMAL_TEXTURE;
+uniform sampler2D SPECULAR_TEXTURE;
+
+uniform sampler2D DIRECTIONAL_SHADOWMAP_TEXTURE;
+
+#include <includes/shadowmapping.common>
+
+void main()
+{
+    vec4 albedoTexture = texture(ALBEDO_TEXTURE, TexCoord);
+
+    FragColor = albedoTexture;
+};
+        
